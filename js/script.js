b9ecb285e1cf349f7cfd1900890f7509724b52ee
Alampi Kevin <Alampi.kevin@gmail.com>
2022-01-05 16:02:49 +0100

    done

--- js/script.js
+++ js/script.js
@@ -2,20 +2,15 @@ let article = document.querySelectorAll('.section--bloc')
 
 .forEach(article => {
     article.addEventListener('mouseover', () => {
-
-    article.style.backgroundColor="black";
-    article.style.color="white";
-    article.style.transition="1s"
+        article.style.backgroundColor="black";
+        article.style.color="white";
+        article.style.transition="1s"
     });
     
     article.addEventListener('mouseout', () =>{
-
         article.style.backgroundColor="white";
         article.style.color="black";
     });
-
-    
-    
 });
 
 
