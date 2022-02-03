94b2851d54bc7fa597610119c8c3ca17ef85ed2c
Alampi Kevin <Alampi.kevin@gmail.com>
2022-02-03 13:03:34 +0100

    start

---
+++ site/views/header.html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href='./css/style.css'>
+    <title>ECF Alampi Kevin</title>
+</head>
+<body>
+    <header>
+        <nav class="navBar">
+            <img class="navBar__logo--afpa" src="image/layout_set_logo.png" alt="logo Afpa">
+            <ul class="navBar--ancre">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Page 1</a></li>
+                <li><a href="#">Page 2</a></li>
+                <li><a href="#">Page 3</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ site/views/main.html.php
@@ -0,0 +1,82 @@
+<main>
+        <article>
+            <section class="section">
+                <article class="section--bloc">
+                    <div class="section__bloc--1" id="bloc1">
+                        <h1>Article 1</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eveniet libero earum facilis, vel impedit, explicabo dolorem quae nulla voluptas velit maxime sapiente fugiat accusantium neque porro tenetur, esse eaque! Officia at corporis cupiditate nam repudiandae explicabo eum debitis placeat quia saepe, tempore similique neque voluptatum velit accusamus iste eos. ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur minima ratione in ducimus vero officia, tenetur quisquam corporis animi doloremque tempore. Suscipit, nihil. Dolore magnam, quasi maxime earum enim pariatur rem veritatis, quibusdam neque fuga similique labore amet ab a maiores ad. Itaque impedit dolore, enim a consectetur
+                        </p>
+                    </div>
+                </article>
+                
+                <article class="section--bloc">
+                    <div class="section__bloc--2">
+                        <h1>Article 2</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium nihil voluptatem officiis necessitatibus tenetur nostrum deleniti dolorum totam quis autem! ipsum dolor sit amet, consectetur adipisicing elit. Commodi tenetur, iusto quos porro odit delectus quidem culpa inventore dolore minima. ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur minima ratione in ducimus vero officia, tenetur quisquam corporis animi doloremque tempore. Suscipit, nihil. Dolore magnam, quasi maxime earum enim pariatur rem veritatis, quibusdam neque fuga similique labore amet ab a maiores ad. Itaque impedit dolore, enim a consectetur officia.</p>
+                    </div>
+                </article>
+                
+                <article class="section--bloc">
+                    <div class="section__bloc--3">
+                        <h1>Article 3</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur minima ratione in ducimus vero officia, tenetur quisquam corporis animi doloremque tempore. Suscipit, nihil. Dolore magnam, quasi maxime earum enim pariatur rem veritatis, quibusdam neque fuga similique labore amet ab a maiores ad. Itaque impedit dolore, enim a consectetur officia.</p>
+                    </div>
+                </article>
+                
+                <article class="section--bloc">
+                    <div class="section__bloc--4" id="article4">
+                        <h1>Article 4</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aliquid sit officia atque fugit labore necessitatibus quod laboriosam amet aut inventore ipsam eaque porro veritatis, maxime omnis voluptas. Saepe temporibus eum perferendis, laborum iste rerum amet quia optio natus pariatur consequatur odit, consequuntur possimus ad cupiditate id, aut maiores. Error eveniet vero nulla ut! Fugit, at. Dolor eaque rerum voluptatem dicta ipsam? Harum voluptate possimus quasi cupiditate! Optio officia ipsam aliquam, nisi sunt tempora eos vitae quasi aut quia similique veritatis, impedit accusantium illum. Laboriosam, vero. Cum vel nemo repellendus necessitatibus laudantium, alias repudiandae obcaecati consequuntur distinctio id quasi labore? ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur minima ratione in ducimus vero officia, tenetur quisquam corporis animi doloremque tempore. Suscipit, nihil. Dolore magnam, quasi maxime earum enim pariatur rem veritatis, quibusdam neque fuga similique labore amet ab a maiores ad. Itaque impedit dolore, enim a consectetur officia.</p>
+                    </div>
+                </article>
+                
+                <article class="section--bloc">
+                    <div class="section__bloc--5" id="article5">
+                        <h1>Article 5</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore odit quae delectus, quasi hic suscipit facilis optio aspernatur natus obcaecati amet, velit adipisci blanditiis voluptatem, itaque ratione libero! Doloribus, maiores. ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur minima ratione in ducimus vero officia, tenetur quisquam corporis animi doloremque tempore. Suscipit, nihil. Dolore magnam, quasi maxime earum enim pariatur rem veritatis, quibusdam neque fuga similique labore amet ab a maiores ad. Itaque impedit dolore, enim a consectetur officia.</p>
+                    </div>
+                </article>
+                
+                <article class="section--bloc">
+                    <div class="section__bloc--6" id="article6">
+                        <h1>Article 6</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore quae at, dicta quisquam ullam expedita impedit sequi eaque nostrum eveniet vel ad alias non vitae incidunt deleniti ex minus eos, consequatur aperiam totam. Totam quam culpa a inventore dolor. Laboriosam exercitationem adipisci ea, autem deleniti repellendus officia voluptate modi repudiandae provident aliquid asperiores, fugit repellat nobis et a cumque hic odit non dolor ipsam? At quo voluptatem nemo praesentium in doloremque rem ratione omnis delectus! Minus aut omnis ab? Animi saepe veniam rem pariatur repellat molestias, perferendis inventore accusantium quod ipsum libero minus quia dolorum debitis totam dolorem asperiores omnis facere voluptate est. Ea, optio doloremque? Eveniet sequi atque accusantium facilis quo perspiciatis iste, rerum consequuntur corrupti est officia dicta. Adipisci, fugit id sunt veniam enim quos molestias a sequi rerum, debitis itaque nihil mollitia aperiam delectus similique maiores tempore numquam, animi illum assumenda aut ex. Reprehenderit, laborum at ut ab quibusdam velit saepe iure molestiae fugiat nihil delectus dignissimos? Aperiam veniam eligendi nostrum. Possimus, officiis quis qui corrupti expedita cumque provident sed tempora reiciendis animi quidem quasi necessitatibus alias nisi commodi quia debitis esse incidunt ad doloremque, sint quam deserunt consequatur. Dicta totam labore rem illo asperiores aliquam laborum! ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur minima ratione in ducimus vero officia, tenetur quisquam corporis animi doloremque tempore. Suscipit, nihil. Dolore magnam, quasi maxime earum enim pariatur rem veritatis, quibusdam neque fuga similique labore amet ab a maiores ad. Itaque impedit dolore, enim a consectetur officia.</p>
+                    </div>
+                </article>
+            </section>
+        </article>
+        
+        <aside>
+            <div class="aside">
+                <div class="aside--bloc">
+                    <nav class="aside__bloc--nav">
+                        <ul>
+                            <li><a class="a" href="#article4">Article 4</a></li>
+                            <li><a class="a" href="#article5">Article 5</a></li>
+                            <li><a class="a" href="#article6">Article 6</a></li>
+                            
+                        </ul>
+                    </nav>
+                </div>
+                
+                <div class="aside--bloc">   
+                    <div class="aside__bloc--video">
+                        <iframe src="https://giphy.com/embed/jzLG3behPzsBl4uvtW" width="100%" height="100%" frameborder ="0" class="giphy-embed" allowfullscreen> </iframe>
+                    </div>
+                </div>
+                
+                <div class="aside--bloc">
+                    <form class="aside__bloc--form" action="">
+                        <div>
+                            <label for="email">Envoyer un email<br></label>
+                            <input type="email" name="email" id="email" required>
+                            <button><a href="mailto: Alampi.kevin@gmail.com?">Envoyer</a></button>
+                            
+                            
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+        </aside>
+    </main>
